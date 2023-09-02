8753f0adc32f1fa4df65f62f7333a2b4d9f7172a
oqwif <jimconroy.pm@gmail.com>
2023-09-02 23:32:35 +1000

    added other models for azure

--- Sources/OpenAISwift/Models/OpenAIModelType.swift
+++ Sources/OpenAISwift/Models/OpenAIModelType.swift
@@ -109,6 +109,10 @@ public enum OpenAIModelType {
         /// > Model Name: gpt-3.5-turbo
         case chatgpt = "gpt-3.5-turbo"
         
+        case gpt35 = "gpt35"
+        
+        case gpt4 = "gpt4"
+        
         /// Snapshot of gpt-3.5-turbo from March 1st 2023. Unlike gpt-3.5-turbo, this model will not receive updates, and will only be supported for a three month period ending on June 1st 2023.
         /// > Model Name: gpt-3.5-turbo-0301
         case chatgpt0301 = "gpt-3.5-turbo-0301"
